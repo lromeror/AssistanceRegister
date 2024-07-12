d1891978edb690c721284f4dec34460d0cb04077
Luis Romero <lromeror@fiec.espol.edu.ec>
2024-07-12 01:59:25 -0500

    changes de form user

--- src/FormularioUsuario.js
+++ src/FormularioUsuario.js
@@ -70,7 +70,148 @@ const FormularioUsuario = ({ selectedUser }) => {
   };
 
   return (
-    
+    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <h2>Información del Usuario</h2>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="nombre"
+            label="Nombre"
+            fullWidth
+            value={usuario.nombre}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="apellido"
+            label="Apellido"
+            fullWidth
+            value={usuario.apellido}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="telefono_movil"
+            label="Teléfono Móvil"
+            fullWidth
+            value={usuario.telefono_movil}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="correo_electronico"
+            label="Correo Electrónico"
+            fullWidth
+            value={usuario.correo_electronico}
+            onChange={handleChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            name="ocupacion"
+            label="Ocupación"
+            select
+            fullWidth
+            value={usuario.ocupacion}
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>Select...</em>
+            </MenuItem>
+            <MenuItem value="Estudiante">Estudiante</MenuItem>
+            <MenuItem value="Profesional">Profesional</MenuItem>
+          </TextField>
+        </Grid>
+        {usuario.ocupacion === 'Estudiante' && (
+          <>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="carrera"
+                label="Carrera"
+                fullWidth
+                value={usuario.carrera}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="universidad"
+                label="Universidad"
+                fullWidth
+                value={usuario.universidad}
+                onChange={handleChange}
+              />
+            </Grid>
+          </>
+        )}
+        {usuario.ocupacion === 'Profesional' && (
+          <>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="organizacion"
+                label="Organización"
+                fullWidth
+                value={usuario.organizacion}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="trabajo"
+                label="Trabajo"
+                fullWidth
+                value={usuario.trabajo}
+                onChange={handleChange}
+              />
+            </Grid>
+          </>
+        )}
+        <Grid item xs={12} sm={3}>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={usuario.asistencia}
+                onChange={handleChange}
+                name="asistencia"
+              />
+            }
+            label="Asistencia"
+          />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={usuario.comida}
+                onChange={handleChange}
+                name="comida"
+              />
+            }
+            label="Comida"
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Enviar Actualización
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Button variant="contained" color="secondary" fullWidth>
+            Eliminar Registro
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Button variant="contained" onClick={handleClear} fullWidth>
+            Limpiar Campos
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
